485baf7e3cafe747d6ecda0d45f71da4fe07eb90
Matt Boutell <boutell@gmail.com>
2017-07-11 11:01:38 -0400

    Starting code added

---
+++ MovieQuotes/app/src/main/java/edu/rosehulman/moviequotes/MovieQuote.java
@@ -0,0 +1,30 @@
+package edu.rosehulman.moviequotes;
+
+/**
+ * Created by Matt Boutell on 12/15/2015, based on earlier work by Dave Fisher.
+ */
+public class MovieQuote {
+    private String quote;
+    private String movie;
+
+    public MovieQuote(String quote, String movie) {
+        this.movie = movie;
+        this.quote = quote;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public String getMovie() {
+        return movie;
+    }
+
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+}
